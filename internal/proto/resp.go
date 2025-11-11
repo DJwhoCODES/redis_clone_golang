feb97035f067b945d30b064530afac776738d699
DJwhoCODES <jaindevanshu824@gmail.com>
2025-11-11 21:25:48 +0530

    built the cmd handler & proto file

--- internal/proto/resp.go
+++ internal/proto/resp.go
@@ -1,1 +1,43 @@
 package proto
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strconv"
+	"strings"
+)
+
+func Parse(r *bufio.Reader) ([]string, error) {
+	line, err := r.ReadString('\n')
+	if err != nil {
+		return nil, err
+	}
+	line = strings.TrimSpace(line)
+
+	if !strings.HasPrefix(line, "*") {
+		return strings.Split(line, " "), nil
+	}
+
+	n, _ := strconv.Atoi(line[1:])
+	args := make([]string, 0, n)
+
+	for i := 0; i < n; i++ {
+		_, _ = r.ReadString('\n')    // skip $len
+		val, _ := r.ReadString('\n') // read value
+		args = append(args, strings.TrimSpace(val))
+	}
+	return args, nil
+}
+
+func WriteSimple(conn net.Conn, msg string) {
+	conn.Write([]byte("+" + msg + "\r\n"))
+}
+
+func WriteError(conn net.Conn, msg string) {
+	conn.Write([]byte("-ERR " + msg + "\r\n"))
+}
+
+func WriteBulk(conn net.Conn, msg string) {
+	conn.Write([]byte(fmt.Sprintf("$%d\r\n%s\r\n", len(msg), msg)))
+}
--- internal/server/handler.go
+++ internal/server/handler.go
@@ -1,1 +1,62 @@
 package server
+
+import (
+	"net"
+	"strings"
+
+	"github.com/djwhocodes/redis-clone/internal/proto"
+	"github.com/djwhocodes/redis-clone/internal/store"
+)
+
+type Handler struct {
+	store *store.Store
+}
+
+func NewHandler(s *store.Store) *Handler {
+	return &Handler{store: s}
+}
+
+func (h *Handler) Handle(conn net.Conn, args []string) {
+	if len(args) == 0 {
+		proto.WriteError(conn, "empty command")
+		return
+	}
+
+	cmd := strings.ToUpper(args[0])
+
+	switch cmd {
+	case "PING":
+		proto.WriteSimple(conn, "PONG")
+
+	case "SET":
+		if len(args) != 3 {
+			proto.WriteError(conn, "wrong number of arguments for SET")
+			return
+		}
+		h.store.Set(args[1], args[2])
+		proto.WriteSimple(conn, "OK")
+
+	case "GET":
+		if len(args) != 2 {
+			proto.WriteError(conn, "wrong number of arguments for GET")
+			return
+		}
+		val, ok := h.store.Get(args[1])
+		if !ok {
+			proto.WriteBulk(conn, "")
+			return
+		}
+		proto.WriteBulk(conn, val)
+
+	case "DEL":
+		if len(args) != 2 {
+			proto.WriteError(conn, "wrong number of arguments for DEL")
+			return
+		}
+		h.store.Del(args[1])
+		proto.WriteSimple(conn, "OK")
+
+	default:
+		proto.WriteError(conn, "unknown command "+cmd)
+	}
+}
