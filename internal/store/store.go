785a09e7d1cb7fa5665c9f9838ba284c6bf727a3
DJwhoCODES <jaindevanshu824@gmail.com>
2025-11-11 21:16:48 +0530

    logger config & built store

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,1 +1,31 @@
 package store
+
+import "sync"
+
+type Store struct {
+	data map[string]string
+	mu   sync.RWMutex
+}
+
+func NewStore() *Store {
+	return &Store{data: make(map[string]string)}
+}
+
+func (s *Store) Set(key, value string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.data[key] = value
+}
+
+func (s *Store) Get(key string) (string, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	val, ok := s.data[key]
+	return val, ok
+}
+
+func (s *Store) Del(key string) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	delete(s.data, key)
+}
